201da6966cae2c8a37798db0cf5cbc575482880b
laiba-shakeel <aqsa.shaqil@gmail.com>
2023-05-24 17:45:15 +0500

    Create components, Modules and learn angular routing system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'blog'
+  Name = 'Laiba shakeel'
+  getName() 
+  {
+    return this.Name
+  }
+  obj={
+    name:'Lareb', 
+    age: '20y'
+  }
+  siteUrl= window.location.href
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule , Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import {UsersModule} from './users/users.module'
+// const routes: Routes = [
+//   {path: 'home' , component: HomeComponent},
+//   {path: '' , redirectTo: '/home', pathMatch: 'full'}
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserListComponent, 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule.forRoot(routes)
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
